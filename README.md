e47469f3bb0fa38c3dcd79812add313b1b9d6ea1
Jens Reimann <jreimann@redhat.com>
2020-09-29 11:33:02 +0200

    Trying to be a bit more creative

---
+++ README.md
@@ -0,0 +1,9 @@
+# About `ctron`
+
+Simply run:
+
+    cargo install ctron
+
+Followed by:
+
+    ctron
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use colored::*;
+
 fn main() {
-    println!("Cheers ctron!");
+    println!(
+        r#"
+{header}
+
+  {about}
+
+{i_am}
+
+{b} 🚧 working on {working_on}
+{b} ✨ a {rust} fanboy
+{b} 🐦 tweeting on Twitter as {twitter}
+{b} 💡 collaborating on GitHub as {github} 
+{b} 🇩🇪 located in {location}
+{b} 🔗 blogging at {url}
+
+"#,
+        b = " »".dimmed(),
+        header = "»»»» About me ««««".bold(),
+        about = "Working on open source, cloud scale IoT messaging".italic(),
+        i_am = "›››› I am ‹‹‹‹".bold(),
+        working_on = "IoT stuff".bold(),
+        rust = "Rust".bold(),
+        twitter = "@ctron".bold(),
+        github = "@ctron".bold(),
+        location = "Germany".bold(),
+        url = "https://dentrassi.de".underline(),
+    );
 }
